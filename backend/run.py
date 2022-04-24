74ed6998251b054020976122ee8d65ba06745998
kioenguyen0207 <nguyendinhquy2000@gmail.com>
2022-04-24 16:44:37 +0700

    update port to deploy

--- backend/run.py
+++ backend/run.py
@@ -6,8 +6,8 @@ api = Api(app)
 
 #endpoint(s)
 api.add_resource(Relatives, "/relatives")
-api.add_resource(TestJson, "/cicd_test")
+# api.add_resource(TestJson, "/cicd_test")
 
 if __name__=='__main__':
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=3306)
     # app.run()
